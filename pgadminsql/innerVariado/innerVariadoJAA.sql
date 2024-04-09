75ec80b6098a1b5ea77e4f792c78580b8e6ddf90
joselitor <tidusasdeloszanarkandabes@gmail.com>
2024-04-09 19:02:00 +0200

    inner variado

--- pgadminsql/innerVariado/innerVariadoJAA.sql
+++ pgadminsql/innerVariado/innerVariadoJAA.sql
@@ -41,18 +41,23 @@ INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',
  * 1. DEVUELVE UN LISTADO CON LOS EMPLEADOS Y LOS DATOS DE LOS DEPARTAMENTOS DONDE *
  *                                TRABAJA CADA UNO                                 *
  ***********************************************************************************/
+ SELECT empleado.nombre, apellido1, apellido2, departamento.nombre AS "nombreDepartamento", departamento.presupuesto, departamento.gastos  FROM empleado INNER JOIN departamento ON empleado.id_departamento = departamento.id;
 
 /***************************************************************************************
- *  2.  DEVUELVE UN LISTADO CON LOS EMPLEADOS Y LOS DATOS DE LOS DEPARTAMENTOS DONDE   *
+ *  2. DEVUELVE UN LISTADO CON LOS EMPLEADOS Y LOS DATOS DE LOS DEPARTAMENTOS DONDE    *
  *      TRABAJA CADA UNO. ORDENA EL RESULTADO, EN PRIMER LUGAR, POR EL NOMBRE DEL      *
  * DEPARTAMENTO (EN ORDEN ALFABÉTICO) Y EN SEGUNDO LUGAR POR LOS APELLIDOS Y EL NOMBRE *
  *                                  DE LOS EMPLEADOS                                   *
  ***************************************************************************************/
+ SELECT empleado.nombre, apellido1, apellido2, departamento.nombre AS "nombreDepartamento", departamento.presupuesto, departamento.gastos  FROM empleado INNER JOIN departamento ON empleado.id_departamento = departamento.id ORDER BY departamento.nombre ASC, apellido1 ASC;
 
 /******************************************************************************************
  * 3. DEVUELVE UN LISTADO CON EL IDENTIFICADOR Y EL NOMBRE DEL DEPARTAMENTO, SOLAMENTE DE *
  *                      AQUELLOS DEPARTAMENTOS QUE TIENEN EMPLEADOS                       *
  ******************************************************************************************/
+ SELECT DISTINCT departamento.id, departamento.nombre FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id ORDER BY id ASC;
+ /*Una solución que vi un poco más limpia y más clara, aquí no pongo el id, te escupe directamente los departamentos y ya, el otro es muy elegante xD*/
+ SELECT DISTINCT departamento.nombre FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id;
 
 /******************************************************************************************
  * 4. DEVUELVE UN LISTADO CON EL IDENTIFICADOR, EL NOMBRE DEL DEPARTAMENTO Y EL VALOR DEL *
@@ -61,33 +66,41 @@ INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',
  *  DEL PRESUPUESTO INICIAL (COLUMNA PRESUPUESTO) EL VALOR DE LOS GASTOS QUE HA GENERADO  *
  *                                    (COLUMNA GASTOS)                                    *
  ******************************************************************************************/
+ SELECT DISTINCT departamento.nombre, (departamento.presupuesto-departamento.gastos) AS presupuestoActual FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id;
 
 /*************************************************************************************
  * 5. DEVUELVE EL NOMBRE DEL DEPARTAMENTO DONDE TRABAJA EL EMPLEADO QUE TIENE EL NIF *
  *                                     38382980M                                     *
  *************************************************************************************/
+ SELECT departamento.nombre FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id WHERE nif LIKE '38382980M';
 
 /**************************************************************************************
  * 6. DEVUELVE EL NOMBRE DEL DEPARTAMENTO DONDE TRABAJA EL EMPLEADO PEPE RUIZ SANTANA *
  **************************************************************************************/
+ SELECT departamento.nombre FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id WHERE empleado.nombre LIKE 'Pepe' AND empleado.apellido1 LIKE 'Ruiz' AND empleado.apellido2 LIKE 'Santana';
 
 /*****************************************************************************************
  * 7. DEVUELVE UN LISTADO CON LOS DATOS DE LOS EMPLEADOS QUE TRABAJAN EN EL DEPARTAMENTO *
  *                      DE I+D. ORDENA EL RESULTADO ALFABÉTICAMENTE                      *
  *****************************************************************************************/
+ SELECT empleado.nombre, empleado.apellido1, empleado.apellido2 AS "empleado" FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id WHERE departamento.nombre LIKE 'I+D';
 
 /*****************************************************************************************
  * 8. DEVUELVE UN LISTADO CON LOS DATOS DE LOS EMPLEADOS QUE TRABAJAN EN EL DEPARTAMENTO *
  *         DE SISTEMAS, CONTABILIDAD O I+D. ORDENA EL RESULTADO ALFABÉTICAMENTE          *
  *****************************************************************************************/
+ SELECT departamento.nombre AS "Departamento", empleado.nombre, empleado.apellido1, empleado.apellido2 AS "empleado" FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id WHERE departamento.nombre LIKE 'I+D' OR departamento.nombre LIKE 'Sistemas' OR departamento.nombre LIKE 'Contabilidad' ORDER BY departamento.nombre ASC; 
 
 /*****************************************************************************************
  * 9. DEVUELVE UNA LISTA CON EL NOMBRE DE LOS EMPLEADOS QUE TIENEN LOS DEPARTAMENTOS QUE *
  *                 NO TIENEN UN PRESUPUESTO ENTRE 100000 Y 200000 EUROS                  *
  *****************************************************************************************/
+ SELECT departamento.nombre AS "departamento", empleado.nombre, empleado.apellido1, empleado.apellido2 FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id WHERE departamento.presupuesto BETWEEN 100000 AND 200000 ORDER BY departamento.nombre ASC;
 
 /*********************************************************************************
  * 10. DEVUELVE UN LISTADO CON EL NOMBRE DE LOS DEPARTAMENTOS DONDE EXISTE ALGÚN *
  * EMPLEADO CUYO SEGUNDO APELLIDO SEA NULL. TENGA EN CUENTA QUE NO DEBE MOSTRAR  *
  *                 NOMBRES DE DEPARTAMENTOS QUE ESTÉN REPETIDOS                  *
  *********************************************************************************/
+ /*En este ejercicio daría igual hacer el distinct, porque no se da el caso de que hayan dos apellido2 nulos en contabilidad y sistemas, podría quitar el distinct y daría lo mismo XD*/
+ SELECT DISTINCT departamento.nombre FROM departamento INNER JOIN empleado ON empleado.id_departamento = departamento.id WHERE empleado.apellido2 IS NULL;
